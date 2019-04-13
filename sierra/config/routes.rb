a95601f260981b04b21ffd9c6ebb5b8bf72c1ac1
Nick Hagerty <nhagerty1@gmail.com>
2019-04-12 20:19:27 -0400

    added user functionality - still need to add admin

--- sierra/config/routes.rb
+++ sierra/config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+
+  get 'login', to: 'sessions#new', as: 'login'
+
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :games do
     resources :images
 	collection do
 		get :search
 	end
   end
+  
 end
 
--- sierra/db/schema.rb
+++ sierra/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190411232802) do
+ActiveRecord::Schema.define(version: 20190412231732) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20190411232802) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_images_on_game_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
