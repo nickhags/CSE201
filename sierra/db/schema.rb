7d9e2dfabe21a5fa604e7d8ea86d55a07db171c9
Nick Hagerty <nhagerty1@gmail.com>
2019-04-26 17:21:11 -0400

    Implemented Javascript sort, page refresh undoes sorting

--- sierra/db/schema.rb
+++ sierra/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190422214856) do
+ActiveRecord::Schema.define(version: 20190426170032) do
 
   create_table "applications", force: :cascade do |t|
     t.string "title"
