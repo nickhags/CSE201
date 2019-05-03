84395e03a35e8923d2fa55d7ea72e1f21edc5392
Nick Hagerty <nhagerty1@gmail.com>
2019-05-02 21:29:59 -0400

    last commit before merge

--- sierra/db/schema.rb
+++ sierra/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190502210140) do
+ActiveRecord::Schema.define(version: 20190503002702) do
 
   create_table "applications", force: :cascade do |t|
     t.string "title"
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 20190502210140) do
     t.float "price"
     t.string "status"
     t.text "comments"
-    t.text "images"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "images"
   end
 
   create_table "games", force: :cascade do |t|
